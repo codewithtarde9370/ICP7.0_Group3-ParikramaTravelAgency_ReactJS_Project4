c563494c9241a859fc80a2cc9299cc13881ab1d1
codewithtarde9370 <teja36491@gmail.com>
2024-06-16 17:26:13 +0530

    added responsive footer

--- src/views/stays/stays.js
+++ src/views/stays/stays.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import Footer from '../../components/footer/footer';
-import'./stays.css';
-import SearchCard from './../../components/search-card/search'
+import './stays.css';
+import SearchCard from './../../components/search-card/search';
 
 function Stays() {
   return (
     <>
-    <Navbar className="navbar" />
-    <div  className='background'>
-      <SearchCard/>
-    </div>
-    <Footer className="footer"/>
-
+      <Navbar className="navbar" />
+      <div className='background'>
+        <SearchCard />
+      </div>
+      <Footer className="footer" />
     </>
-  )
+  );
 }
 
-export default Stays
+export default Stays;
